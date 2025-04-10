e736f0ba34830117b658dd453e28fb007e6a123a
MariaMartinezRos <cibernuggets@gmail.com>
2025-04-10 15:36:00 +0200

    Añadido campo tabla Managers

--- database/migrations/2025_03_31_174729_create_managers_table.php
+++ database/migrations/2025_03_31_174729_create_managers_table.php
@@ -9,6 +9,7 @@ return new class extends Migration {
     {
         Schema::create('managers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('tasca_id')->constrained('tascas')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });

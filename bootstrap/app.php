f4b02c434ef6acf8107502c12aef559aa9953768
PeterCavv <pedroabad52@gmail.com>
2025-05-02 14:16:45 +0200

    Configuración para funcionar API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,23 +1,59 @@
 <?php
 
+use App\Http\Middleware\AlwaysAcceptJsonMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+//        $middleware->web(append: [
+//            \App\Http\Middleware\HandleInertiaRequests::class,
+//            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+//        ]);
 
-        //
+        $middleware->prependToGroup('api', AlwaysAcceptJsonMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (ValidationException $e, Request $request) {
+            return response()->json([
+                'error' => 'Validation failed',
+                'message' => $e->getMessage(),
+                'errors' => $e->errors(),
+            ], 422);
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                'error' => 'Not found',
+                'message' => $e->getMessage(),
+            ], 404);
+        });
+
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
+            return response()->json([
+                'error' => 'Not found',
+                'message' => $e->getMessage(),
+            ], 500);
+        });
+
+//        $exceptions->renderable(function (UnauthorizedException $e) {
+//            return response()->json([
+//                'error' => 'Unauthorized',
+//                'message' => $e->getMessage(),
+//            ], 403);
+//        });
+
+        return response()->json([
+            'error' => 'Internal Server Error'
+        ], 500);
     })->create();

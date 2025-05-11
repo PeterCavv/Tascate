6679af0ae126eac9ca373637ca5bdedcb80dad3a
MariaMartinezRos <cibernuggets@gmail.com>
2025-05-11 19:29:30 +0200

    add roles with laravel/permission

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Tasca Management Permissions
+        Permission::create(['name' => 'delete tasca']);
+        Permission::create(['name' => 'edit tasca settings']);
+        Permission::create(['name' => 'delete tasca settings']);
+        Permission::create(['name' => 'view tasca statistics']);
+        Permission::create(['name' => 'view tasca menu']);
+        Permission::create(['name' => 'edit tasca menu']);
+        Permission::create(['name' => 'delete tasca menu']);
+        Permission::create(['name' => 'view tasca hours']);
+        Permission::create(['name' => 'edit tasca hours']);
+        Permission::create(['name' => 'delete tasca hours']);
+
+        // Employee Management Permissions
+        Permission::create(['name' => 'edit employees']);
+        Permission::create(['name' => 'delete employees']);
+        Permission::create(['name' => 'view employees']);
+        Permission::create(['name' => 'assign employees']);
+
+        // Reservation Management Permissions
+        Permission::create(['name' => 'edit reservations']);
+        Permission::create(['name' => 'delete reservations']);
+        Permission::create(['name' => 'view reservations']);
+        Permission::create(['name' => 'create reservations']);
+
+        // Review Management Permissions
+        Permission::create(['name' => 'edit reviews']);
+        Permission::create(['name' => 'delete reviews']);
+        Permission::create(['name' => 'view reviews']);
+        Permission::create(['name' => 'create reviews']);
+
+        // Post Management Permissions
+        Permission::create(['name' => 'edit posts']);
+        Permission::create(['name' => 'delete posts']);
+        Permission::create(['name' => 'create posts']);
+        Permission::create(['name' => 'view posts']);
+
+        // User Management Permissions
+        Permission::create(['name' => 'edit users']);
+        Permission::create(['name' => 'delete users']);
+        Permission::create(['name' => 'view users']);
+
+        // System Management Permissions
+        Permission::create(['name' => 'edit system']);
+        Permission::create(['name' => 'delete system']);
+        Permission::create(['name' => 'view system logs']);
+    }
+
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Create roles
+        $admin = Role::create(['name' => 'Admin']);
+        $owner = Role::create(['name' => 'Owner']);
+        $manager = Role::create(['name' => 'Manager']);
+        $employee = Role::create(['name' => 'Employee']);
+        $tasca = Role::create(['name' => 'Tasca']);
+
+        // Admin gets all permissions
+        $admin->givePermissionTo(Permission::all());
+
+        // Owner permissions
+        $owner->givePermissionTo([
+            'delete tasca',
+            'edit tasca settings',
+            'delete tasca settings',
+            'view tasca statistics',
+            'view tasca menu',
+            'edit tasca menu',
+            'delete tasca menu',
+            'view tasca hours',
+            'edit tasca hours',
+            'delete tasca hours',
+            'edit employees',
+            'delete employees',
+            'view employees',
+            'assign employees',
+            'edit reservations',
+            'delete reservations',
+            'view reservations',
+            'edit reviews',
+            'delete reviews',
+            'view reviews',
+            'edit posts',
+            'delete posts',
+            'create posts',
+            'view posts',
+        ]);
+        
+        // Manager permissions
+        $manager->givePermissionTo([
+            'view tasca statistics',
+            'view tasca menu',
+            'view tasca hours',
+            'view employees',
+            'edit reservations',
+            'delete reservations',
+            'view reservations',
+            'edit reviews',
+            'delete reviews',
+            'view reviews',
+            'create posts',
+            'delete posts',
+            'view posts',
+        ]);
+        
+        // Employee permissions
+        $employee->givePermissionTo([
+            'view tasca menu',
+            'view tasca hours',
+            'view reservations',
+            'create reservations',
+            'view reviews',
+            'create reviews',
+            'view posts',
+        ]);
+        
+        // Tasca permissions
+        $tasca->givePermissionTo([
+            'delete tasca',
+            'edit tasca settings',
+            'delete tasca settings',
+            'view tasca statistics',
+            'view tasca menu',
+            'edit tasca menu',
+            'delete tasca menu',
+            'view tasca hours',
+            'edit tasca hours',
+            'delete tasca hours',
+            'edit employees',
+            'delete employees',
+            'view employees',
+            'assign employees',
+            'edit reservations',
+            'delete reservations',
+            'view reservations',
+            'edit reviews',
+            'delete reviews',
+            'view reviews',
+            'edit posts',
+            'delete posts',
+            'create posts',
+            'view posts',
+        ]);
+        
+    }
+}
+
+
+
+
+
+
+
+

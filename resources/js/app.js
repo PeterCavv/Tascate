b47615d719d1fbefda7271eb50931293b9cd5cc3
MariaMartinezRos <cibernuggets@gmail.com>
2025-05-13 19:49:36 +0200

    edit layout A MEDIAS

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,12 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
+// PrimeVue styles
+import 'primevue/resources/themes/lara-light-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Tascate';
 
@@ -20,11 +23,13 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(PrimeVue)
-            .mount(el);
+            .use(PrimeVue, { ripple: true })
+            .component('Button', Button);
+
+        app.mount(el);
     },
     progress: {
         color: '#4B5563',

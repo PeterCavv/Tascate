cc48217318f813b0569327c98d687550e9bc91f8
MariaMartinezRos <cibernuggets@gmail.com>
2025-05-15 09:52:39 +0200

    adjust login view

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,7 +1,6 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
-import { ref } from 'vue';
 
 defineProps({
     canResetPassword: Boolean,

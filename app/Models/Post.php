b8916a4c862fc4589ffe300735f5f95435ffb030
Snippy <168076645+Snippyyy@users.noreply.github.com>
2025-05-19 19:27:27 +0200

    Creación de Observer para Posts
    Manejo de eliminación de imagenes de un Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use App\Observers\PostObserver;
 
+
+#[ObservedBy([PostObserver::class])]
 class Post extends Model
 {
     use HasFactory;

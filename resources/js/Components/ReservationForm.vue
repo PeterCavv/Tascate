5651dc72a5cb7baeca2a33bb4cbf0781a297698a
PeterCavv <pedroabad52@gmail.com>
2025-05-21 17:23:26 +0200

    Adaptado formulario de reservas para editar y crear

--- resources/js/Components/ReservationForm.vue
+++ resources/js/Components/ReservationForm.vue
@@ -3,8 +3,10 @@ import MainLayoutTemp from "@/Layouts/MainLayoutTemp.vue";
 import {useForm, usePage} from '@inertiajs/vue3';
 import {computed} from "vue";
 
-const {tasca} = defineProps({
+const {tasca, isEdit, reservation} = defineProps({
     tasca: Object,
+    isEdit: Boolean,
+    reservation: Object
 });
 
 const { auth } = usePage().props
@@ -12,10 +14,10 @@ const { auth } = usePage().props
 const form = useForm({
     tasca_id: tasca.id,
     reservation_price: tasca.reservation_price,
-    reservation_date: '',
-    reservation_time: '',
-    people: 1,
-    observations: '',
+    reservation_date: reservation ? reservation.reservation_date : '',
+    reservation_time: reservation ? reservation.reservation_time : '',
+    people: reservation ? reservation.people : 1,
+    observations: reservation ? reservation.observations : '',
 });
 
 const openingTimeFormatted = computed(() => tasca.opening_time?.slice(0, 5))
@@ -28,10 +30,17 @@ layout: MainLayoutTemp,
 });
 
 const submitReservation = () => {
-    form.post(route('reservations.store'), {
+    !isEdit ? form.post(route('reservations.store'), {
         forceFormData: true,
         onSuccess: () => {
-            alert('Reserva realizada correctamente.');
+            form.reset();
+        },
+        onError: (errors) => {
+            console.error(errors);
+        },
+    }) : form.put(route('reservations.update', reservation), {
+        forceFormData: true,
+        onSuccess: () => {
             form.reset();
         },
         onError: (errors) => {
@@ -45,7 +54,10 @@ const submitReservation = () => {
 <template>
 
     <div class="max-w-2xl mx-auto p-6 space-y-6">
-        <h2 class="text-2xl font-bold text-gray-800"> Reservar en {{ tasca.name }}</h2>
+        <h2 class="text-2xl font-bold text-gray-800">
+            <span v-if="!isEdit">Reservar en {{ tasca.name }}</span>
+            <span v-else>Reserva en {{ tasca.name }}</span>
+        </h2>
 
         <form @submit.prevent="submitReservation">
             <div class="space-y-4">
@@ -120,13 +132,14 @@ const submitReservation = () => {
                     <button
                         class="px-5 py-1.5 rounded-full bg-green-600 text-white font-semibold shadow-md hover:bg-green-700 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-1"
                     >
-                        Reservar
+                        <span v-if="!isEdit">Reservar</span>
+                        <span v-else>Actualizar Reserva</span>
                     </button>
 
-                    <p class="font-bold text-green-950">Precio de la reserva: {{ tasca.reservation_price }}€</p>
+                    <p v-if="!isEdit" class="font-bold text-green-950">Precio de la reserva: {{ tasca.reservation_price }}€</p>
                 </div>
 
-                <p class="text-xs text-gray-500 leading-relaxed"> El pago de la reserva se realiza a través de una plataforma segura. <br /> Si decides cancelar con más de 2 horas de antelación, te reembolsaremos el importe total de la reserva. En caso contrario, no será posble realizar el reembolso. </p>
+                <p v-if="!isEdit" class="text-xs text-gray-500 leading-relaxed"> El pago de la reserva se realiza a través de una plataforma segura. <br /> Si decides cancelar con más de 2 horas de antelación, te reembolsaremos el importe total de la reserva. En caso contrario, no será posble realizar el reembolso. </p>
             </div>
         </form>
     </div>

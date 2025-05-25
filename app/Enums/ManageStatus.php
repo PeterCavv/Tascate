c82745ea6c8bb3f2acfad4494bfd71082bc0321f
PeterCavv <pedroabad52@gmail.com>
2025-05-25 04:14:29 +0200

    Cambio de nombre de enum

--- app/Enums/ManageStatus.php
+++ app/Enums/ManageStatus.php
@@ -2,11 +2,16 @@
 
 namespace App\Enums;
 
-enum FriendshipStatus: string
+enum ManageStatus: string
 {
     case PENDING = 'pending';
     case ACCEPTED = 'accepted';
+
+    // This status can be only used to block a user
     case BLOCKED = 'blocked';
+
+    // This status can be only used to reject a tasca proposal
+    case REJECTED = 'rejected';
 
     public static function getValues(): array
     {
@@ -14,6 +19,7 @@ enum FriendshipStatus: string
             self::PENDING,
             self::ACCEPTED,
             self::BLOCKED,
+            self::REJECTED,
         ];
     }
 }

09b23a33094e4014cf1b126bcb1b8e7e3001d781
Snippy <168076645+Snippyyy@users.noreply.github.com>
2025-06-11 11:11:31 +0200

    Buscador mapa arreglado

--- resources/js/Pages/Tascas/TascaLocationEdit.vue
+++ resources/js/Pages/Tascas/TascaLocationEdit.vue
@@ -26,7 +26,7 @@ const props = defineProps({
 onMounted(() => {
     if (!window.google) {
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
         script.async = true;
         script.defer = true;
         script.onload = initMap;
